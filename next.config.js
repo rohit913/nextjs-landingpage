d6fae6f65bf101c8648eb35fc85af35395600e74
Rohit sharma <rsharma07feb@gmail.com>
2024-04-12 02:46:51 +0530

    new config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
-  const nextConfig = {
-    reactStrictMode: true,
-    typescript: {
-      ignoreBuildErrors: true,
-    },
-  }
+const nextConfig = {
+  reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  // Use output: export to enable static HTML export
+  output: {
+    export: true,
+  },
+};
 
-  module.exports = nextConfig
+module.exports = nextConfig;
